034486bb2795aa76a4fc7d7c0a16d3895b045e51
lehnenmd4053 <37810108+lehnenmd4053@users.noreply.github.com>
2018-04-16 16:51:49 -0500

    Add files via upload

---
+++ resize.m
@@ -0,0 +1,23 @@
+function output = resize(vector1)
+
+[~, text_2] = xlsread('Top 73 Course List.xlsx');
+text_2 = text_2(:,2:2);
+
+text_2 = string(text_2);
+vector1 = string(vector1);
+
+new_vector = strings;
+counter = 1;
+for ii = 1:length(text_2)
+    for jj = 1:length(vector1)
+        sameBool = isequal(text_2(ii,1), vector1(jj,1));
+        if (sameBool == 1)
+            new_vector(counter, 1) = text_2(ii,1);
+            counter = counter + 1;
+        end
+    end
+end
+
+
+output = new_vector
+end %function
