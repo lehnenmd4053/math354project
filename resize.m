84e4b3bc417cd7c4c443b5073493ba378faed1c8
lehnenmd4053 <37810108+lehnenmd4053@users.noreply.github.com>
2018-05-09 11:02:42 -0500

    Update resize.m

--- resize.m
+++ resize.m
@@ -1,14 +1,20 @@
 %This function simply removes classes that contain missing data in their
-%time columns.  The new_vector is the edited version of the input_vector
+%time columns.  The new_vector that is the output is the edited version 
+%of the input_vector
 
 function output = resize(input_vector)
 
+%%
+%read in the top class file to give something to compare the input vector to
 [~, top_class_text] = xlsread('Top 73 Course List.xlsx');
 top_class_text = top_class_text(:,2:2);
 
 top_class_text = string(top_class_text);
 input_vector = string(input_vector);
 
+%%
+%Compares each class of the input vector, one at a time, to each of the
+%classes in the top course list
 new_vector = strings;
 counter = 1;
 for ii = 1:length(top_class_text)
